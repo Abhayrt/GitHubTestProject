5e33827d773f64cd351b6373935547d02e8d4256
Abhayrt <abhaythakur2631@gmail.com>
2021-09-16 11:51:32 +0530

    Output msg adjusted

--- GitTest/src/com/neosoft/MainClass.java
+++ GitTest/src/com/neosoft/MainClass.java
@@ -3,6 +3,6 @@ package com.neosoft;
 public class MainClass {
 	public static void main(String[] args)
 	{
-     System.out.println("Welcome");
+     System.out.println("Welcome to neosoft");
   }
 }
